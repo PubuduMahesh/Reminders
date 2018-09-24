298b4e0d35bf96b2d0b741fa8453d3bc7186c081
PubuduMahesh <pubuduonsocialnetwork@gmail.com>
2018-09-24 21:00:31 +0530

    Adds new reminder and exit menu options

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -2,8 +2,10 @@ package com.apress.gerber.reminders;
 
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.util.Log;
         import android.view.Menu;
         import android.view.MenuInflater;
+        import android.view.MenuItem;
         import android.widget.ArrayAdapter;
         import android.widget.ListView;
 
@@ -34,4 +36,20 @@ public class RemindersActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_reminders,menu);
         return true;
-    }}
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_new:
+                //create new reminder
+                Log.d(getLocalClassName(),"create new Reminder");
+                return true;
+            case R.id.action_exit:
+                finish();
+                return true;
+             default:
+                 return false;
+        }
+    }
+}
