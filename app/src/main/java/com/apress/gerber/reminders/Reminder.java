c386607d6e11b9cdff48e00304159c231d71d73a
PubuduMahesh <pubuduonsocialnetwork@gmail.com>
2018-09-24 21:09:43 +0530

    add getters and setters for Reminder data model

--- app/src/main/java/com/apress/gerber/reminders/Reminder.java
+++ app/src/main/java/com/apress/gerber/reminders/Reminder.java
@@ -1,4 +1,41 @@
 package com.apress.gerber.reminders;
 
 public class Reminder {
+
+
+    private int mId;
+    private String mContent;
+    private int mImportant;
+
+    public Reminder(int id, String content, int important) {
+        mId = id;
+        mImportant = important;
+        mContent = content;
+    }
+
+    public int getmId() {
+        return mId;
+    }
+
+    public void setmId(int mId) {
+        this.mId = mId;
+    }
+
+    public String getmContent() {
+        return mContent;
+    }
+
+    public void setmContent(String mContent) {
+        this.mContent = mContent;
+    }
+    public int getmImportant() {
+        return mImportant;
+    }
+
+    public void setmImportant(int mImportant) {
+        this.mImportant = mImportant;
+    }
+
+
 }
+
