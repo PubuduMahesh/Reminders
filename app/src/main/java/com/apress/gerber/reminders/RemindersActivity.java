7cd0a7a1c0a83e8d9e576297be29c792a98592b1
PubuduMahesh <pubuduonsocialnetwork@gmail.com>
2018-09-24 10:59:40 +0530

    add menu_reminder for main activity

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -1,7 +1,9 @@
 package com.apress.gerber.reminders;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.Menu;
+        import android.view.MenuInflater;
 
 public class RemindersActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class RemindersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_reminders,menu);
+        return true;
+    }}
