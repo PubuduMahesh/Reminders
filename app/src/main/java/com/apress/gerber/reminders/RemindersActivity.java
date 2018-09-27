9f7060d2fd1423df5816e6a885b0bb478ff249d0
PubuduMahesh <pubuduonsocialnetwork@gmail.com>
2018-09-27 22:44:38 +0530

    Adds SQLite database persistence for reminders and a new color for important reminders

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -1,5 +1,6 @@
 package com.apress.gerber.reminders;
 
+        import android.database.Cursor;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.util.Log;
@@ -12,23 +13,45 @@ package com.apress.gerber.reminders;
 public class RemindersActivity extends AppCompatActivity {
 
     private ListView mListView;
+    private RemindersDbAdapter mDbAdapter;
+    private RemindersSimpleCursorAdapter mCursorAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
         mListView = (ListView) findViewById(R.id.reminders_list_view);
-        //The arrayAdatper is the controller in our
-        //model-view-controller relationship. (controller)
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-        //context
-                this,
-        //layout (view)
+        mListView.setDivider(null);
+        mDbAdapter = new RemindersDbAdapter(this);
+        mDbAdapter.open();
+
+        Cursor cursor = mDbAdapter.fetchAllReminders();
+
+        //from columns defined in the db
+        String[] from = new String[]{
+                RemindersDbAdapter.COL_CONTENT
+        };
+
+        //to the ids of views in the layout.
+        int[] to = new int[]{
+                R.id.row_text
+        };
+
+        mCursorAdapter = new RemindersSimpleCursorAdapter(
+                //context
+                RemindersActivity.this,
+                //the layout of the row
                 R.layout.reminders_row,
-        //row (view)
-                R.id.row_text,
-        //data (model) with bogus data to test our listview
-                new String[]{"first record", "second record", "third record"});
-        mListView.setAdapter(arrayAdapter);
+                //cursor
+                cursor,
+                //form columns defined in the db
+                from,
+                //to the ids of views in the layout
+                to,
+                //flag - not used
+                0);
+
+        mListView.setAdapter(mCursorAdapter);
     }
 
     @Override
