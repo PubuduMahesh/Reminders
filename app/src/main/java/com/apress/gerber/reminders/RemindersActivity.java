506460b310a11d4b53cc8bc3336f3269566b3e30
PubuduMahesh <Pubudu.Meththananda@cambio.se>
2019-05-31 23:32:52 +0100

    added sample reminders manually

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -23,10 +23,16 @@ public class RemindersActivity extends AppCompatActivity {
         mListView = (ListView) findViewById(R.id.reminders_list_view);
         mListView.setDivider(null);
         mDbAdapter = new RemindersDbAdapter(this);
+
         mDbAdapter.open();
+        if (savedInstanceState == null) {
+            //Clear all data
+            mDbAdapter.deleteAllReminders();
+            //Add some data
+            insertSomeReminders();
+        }
 
         Cursor cursor = mDbAdapter.fetchAllReminders();
-
         //from columns defined in the db
         String[] from = new String[]{
                 RemindersDbAdapter.COL_CONTENT
@@ -52,6 +58,24 @@ public class RemindersActivity extends AppCompatActivity {
                 0);
 
         mListView.setAdapter(mCursorAdapter);
+    }
+
+    private void insertSomeReminders() {
+        mDbAdapter.createReminder("Buy Learn Android Studio", true);
+        mDbAdapter.createReminder("Send Dad birthday gift", false);
+        mDbAdapter.createReminder("Dinner at the Gage on Friday", false);
+        mDbAdapter.createReminder("String squash racket", false);
+        mDbAdapter.createReminder("Shovel and salt walkways", false);
+        mDbAdapter.createReminder("Prepare Advanced Android syllabus", true);
+        mDbAdapter.createReminder("Buy new office chair", false);
+        mDbAdapter.createReminder("Call Auto-body shop for quote", false);
+        mDbAdapter.createReminder("Renew membership to club", false);
+        mDbAdapter.createReminder("Buy new Galaxy Android phone", true);
+        mDbAdapter.createReminder("Sell old Android phone - auction", false);
+        mDbAdapter.createReminder("Buy new paddles for kayaks", false);
+        mDbAdapter.createReminder("Call accountant about tax returns", false);
+        mDbAdapter.createReminder("Buy 300,000 shares of Google", false);
+        mDbAdapter.createReminder("Call the Dalai Lama back", true);
     }
 
     @Override
